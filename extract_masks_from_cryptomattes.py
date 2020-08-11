746040c7861854d8936c4eca1a25adfb3f80c05b
Shreeyak <ss.shrek7@gmail.com>
2020-08-11 18:56:28 +0530

    refactored code

--- extract_masks_from_cryptomattes.py
+++ extract_masks_from_cryptomattes.py
@@ -1,9 +1,12 @@
 """Use the cryptomattes from Vray/Blender and extract masks from them"""
 import argparse
+import enum
 import json
 import random
 import struct
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Optional, Tuple, Dict
 
 import Imath
 import OpenEXR
@@ -15,39 +18,106 @@ from exr_info import exr_info
 MASK_THRESHOLD = 0.55 * 255
 
 
-def get_crypto_layer_from_exr(exr_file, imsize):
+class ExrDtype(enum.Enum):
+    FLOAT32 = 0
+    FLOAT16 = 1
+
+
+def get_imsize(exr_file: OpenEXR.InputFile):
+    """Get the height and width of image within and EXR file
+
+    Args:
+        exr_file (OpenEXR.InputFile): The opened EXR file object.
+
+    Returns:
+        int, int: Height, Width of image
+    """
+    header = exr_file.header()
+    dw = header['dataWindow']
+    height = int(dw.max.y - dw.min.y + 1)
+    width = int(dw.max.x - dw.min.x + 1)
+
+    return height, width
+
+
+def exr_channel_to_numpy(exr_file: OpenEXR.InputFile, channel_name: str, reshape: Optional[Tuple[int, int]],
+                         dtype: ExrDtype = ExrDtype.FLOAT32):
+    """Extracts a channel in an EXR file into a numpy array
+
+    Args:
+        exr_file (OpenEXR.InputFile): The opened EXR file object
+        channel_name (str): The name of the channel to be converted to numpy
+        reshape (None or Tuple(height, width): If given, will reshape the 2D numpy array into (height, width)
+        dtype (ExrDtype): Whether the data in channel is of float32 or float16 type
+
+    Returns:
+        numpy.ndarray: The extracted channel in form of numpy array
+    """
+    if dtype == ExrDtype.FLOAT32:
+        point_type = Imath.PixelType(Imath.PixelType.FLOAT)
+        np_type = np.float32
+    else:
+        point_type = Imath.PixelType(Imath.PixelType.HALF)
+        np_type = np.float16
+
+    channel_arr = np.frombuffer(exr_file.channel(channel_name, point_type), dtype=np_type)
+    if reshape:
+        channel_arr = channel_arr.reshape(reshape)
+
+    return channel_arr
+
+
+def get_crypto_layer(exr_file: OpenEXR.InputFile, layer_mapping: exr_info.CryptoLayerMapping):
+    """Extract one of the cryptomatte layers as a 4-channel numpy array.
+    Each cryptomatte layer has 4 channels for RGBA.
+
+    Note:
+        - By default, there are 3 crypto layers in an EXR, corresponding to a Cryptomatte of Level 6.
+          Example: ['cryptomatte_00', 'cryptomatte_01', 'cryptomatte_02'].
+        - The "Level" of a cryptomatte corresponds to the max number of unique objects it can store masks for.
+        - Each cryptomatte layer has 4 channels for RGBA.
+
+    Args:
+        exr_file (OpenEXR.InputFile): The opened EXR file object
+        layer_mapping (exr_info.CryptoLayerMapping): A dataclass containing the exact exr channel name for each of the
+                                                     'RGBA' channels in a cryptomatte layer.
+
+    Returns:
+        numpy.ndarray: The cryptomatte layer. Shape: (H, W, 4)
+    """
+    height, width = get_imsize(exr_file)
+    cr_00 = np.zeros((height, width, 4), dtype=np.float32)
+
+    cr_00[:, :, 0] = exr_channel_to_numpy(exr_file, channel_name=layer_mapping.R, reshape=(height, width))
+    cr_00[:, :, 1] = exr_channel_to_numpy(exr_file, channel_name=layer_mapping.G, reshape=(height, width))
+    cr_00[:, :, 2] = exr_channel_to_numpy(exr_file, channel_name=layer_mapping.B, reshape=(height, width))
+    cr_00[:, :, 3] = exr_channel_to_numpy(exr_file, channel_name=layer_mapping.A, reshape=(height, width))
+
+    return cr_00
+
+
+def get_crypto_layers_from_exr(exr_file: OpenEXR.InputFile):
+    """Extracts all the cryptomatte layers from an EXR file
+
+    Note:
+        - By default, there are 3 crypto layers in an EXR, corresponding to a Cryptomatte of Level 6.
+          Example: ['cryptomatte_00', 'cryptomatte_01', 'cryptomatte_02'].
+        - The "Level" of a cryptomatte corresponds to the max number of unique objects it can store masks for.
+        - Each cryptomatte layer has 4 channels for RGBA.
+
+    Args:
+        exr_file (OpenEXR.InputFile): Opened EXR file object
+
+    Returns:
+        numpy.ndarray: First cryptomatte layer. Shape: (H, W, 4)
+        numpy.ndarray: Second cryptomatte layer. Shape: (H, W, 4)
+        numpy.ndarray: Third cryptomatte layer. Shape: (H, W, 4)
+    """
+    # TODO: Change the number of cryptomatte layers depending on the level of the cryptomatte.
     exr_channels = exr_info.ExrChannels(exr_info.Renderer.VRAY)
-    pt_f32 = Imath.PixelType(Imath.PixelType.FLOAT)
-
-    cr_00 = np.zeros(imsize + (4,), dtype=np.float32)
-    cr_00[:, :, 0] = np.frombuffer(exr_file.channel(exr_channels.cryptomatte_00['R'], pt_f32),
-                                   dtype=np.float32).reshape(imsize)
-    cr_00[:, :, 1] = np.frombuffer(exr_file.channel(exr_channels.cryptomatte_00['G'], pt_f32),
-                                   dtype=np.float32).reshape(imsize)
-    cr_00[:, :, 2] = np.frombuffer(exr_file.channel(exr_channels.cryptomatte_00['B'], pt_f32),
-                                   dtype=np.float32).reshape(imsize)
-    cr_00[:, :, 3] = np.frombuffer(exr_file.channel(exr_channels.cryptomatte_00['A'], pt_f32),
-                                   dtype=np.float32).reshape(imsize)
-
-    cr_01 = np.zeros(imsize + (4,), dtype=np.float32)
-    cr_01[:, :, 0] = np.frombuffer(exr_file.channel(exr_channels.cryptomatte_01['R'], pt_f32),
-                                   dtype=np.float32).reshape(imsize)
-    cr_01[:, :, 1] = np.frombuffer(exr_file.channel(exr_channels.cryptomatte_01['G'], pt_f32),
-                                   dtype=np.float32).reshape(imsize)
-    cr_01[:, :, 2] = np.frombuffer(exr_file.channel(exr_channels.cryptomatte_01['B'], pt_f32),
-                                   dtype=np.float32).reshape(imsize)
-    cr_01[:, :, 3] = np.frombuffer(exr_file.channel(exr_channels.cryptomatte_01['A'], pt_f32),
-                                   dtype=np.float32).reshape(imsize)
-
-    cr_02 = np.zeros(imsize + (4,), dtype=np.float32)
-    cr_02[:, :, 0] = np.frombuffer(exr_file.channel(exr_channels.cryptomatte_02['R'], pt_f32),
-                                   dtype=np.float32).reshape(imsize)
-    cr_02[:, :, 1] = np.frombuffer(exr_file.channel(exr_channels.cryptomatte_02['G'], pt_f32),
-                                   dtype=np.float32).reshape(imsize)
-    cr_02[:, :, 2] = np.frombuffer(exr_file.channel(exr_channels.cryptomatte_02['B'], pt_f32),
-                                   dtype=np.float32).reshape(imsize)
-    cr_02[:, :, 3] = np.frombuffer(exr_file.channel(exr_channels.cryptomatte_02['A'], pt_f32),
-                                   dtype=np.float32).reshape(imsize)
+    cr_00 = get_crypto_layer(exr_file, exr_channels.cryptomatte_00)
+    cr_01 = get_crypto_layer(exr_file, exr_channels.cryptomatte_00)
+    cr_02 = get_crypto_layer(exr_file, exr_channels.cryptomatte_00)
 
     return cr_00, cr_01, cr_02
 
@@ -75,42 +145,42 @@ def get_mask_for_id(float_id, cr_00, cr_01, cr_02):
 
 
 def extract_mask(exr_file):
-
+    # Get the manifest (mapping of object names to hash ids)
     header = exr_file.header()
-    dw = header['dataWindow']
-    width, height = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)
-    imsize = (height, width)
-
     for key in header:
         if '/manifest' in key:
             manifest = json.loads(header[key])
             break
 
+    # Convert hash ids to float ids
     float_ids = {}
     for name, value in manifest.items():
         bytes_val = bytes.fromhex(value)
         float_val = struct.unpack('>f', bytes_val)[0]
         float_ids[name] = float_val
 
-    cr_00, cr_01, cr_02 = get_crypto_layer_from_exr(exr_file, imsize)
+    # Extract the crypto layers from EXR file
+    cr_00, cr_01, cr_02 = get_crypto_layers_from_exr(exr_file)
 
-    mask_list = {}
-    id_mapping = {}
-    for ii, (obj_name, float_id) in enumerate(float_ids.items()):
+    # Extract mask of each object in manifest
+    mask_list = []
+    id_list = []
+    id_mapping = {}  # Mapping the name of each obj to obj id
+    for obj_id, (obj_name, float_id) in enumerate(float_ids.items()):
         mask = get_mask_for_id(float_id, cr_00, cr_01, cr_02)
-        mask_list[obj_name] = mask
-        id_mapping[obj_name] = ii
+        mask_list.append(mask)
+        id_list.append(obj_id)
+        id_mapping[obj_name] = obj_id
 
-    mask_combined = np.zeros(imsize, dtype=np.uint16)
-    mask_combined_rgb = np.zeros(imsize + (3, ), dtype=np.uint8)
-    for obj_name in mask_list:
-        mask = mask_list[obj_name]
-        obj_id = id_mapping[obj_name]
-
+    # Combine all the masks into single mask
+    height, width = get_imsize(exr_file)
+    mask_combined = np.zeros((height, width), dtype=np.uint16)
+    mask_combined_rgb = np.zeros((height, width, 3), dtype=np.uint8)
+    for mask, obj_id in zip(mask_list, id_list):
         mask_combined[mask > MASK_THRESHOLD] = obj_id
+
         # TODO: Generate random hue, keeping saturation and value constant. Then convert HSV to RGB.
-        color = lambda: [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
-        rand_color = color()
+        rand_color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
         mask_combined_rgb[mask > MASK_THRESHOLD, :] = rand_color
 
     return mask_combined, mask_combined_rgb, id_mapping
